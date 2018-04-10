79005b2a5132026df26d386742ac3fbd88db734d
Yang sujin <21500394@handong.edu>
2018-04-10 23:11:29 +0900

    add comment

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -52,9 +52,9 @@ public class SalesReporter {
 		System.out.println("The following had the hightest sales:");//print highest sales people data
 		for(int i=0; i<this.numberOfAssociates; i++) {//add loop for print data
 			if(this.highestSales == team[i].getSales()) {//condition for print highest people
-				System.out.println("Name: "+team[i].getName());
-				System.out.println("Sales: "+team[i].getSales());
-				System.out.println(team[i].getSales()-this.averageSales+" above the average.");
+				System.out.println("Name: "+team[i].getName());//print highest people name
+				System.out.println("Sales: "+team[i].getSales());//print highest people sales
+				System.out.println(team[i].getSales()-this.averageSales+" above the average.");//print over sales
 			}
 		}
 	}
