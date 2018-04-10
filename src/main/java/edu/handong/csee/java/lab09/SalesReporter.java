2b169ee0514bfc384a78d1c01af377a8cfea9bc2
Yang sujin <21500394@handong.edu>
2018-04-10 22:49:11 +0900

    add instance variable and getData method

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,9 +1,15 @@
 package edu.handong.csee.java.lab09;
 import java.util.Scanner;
 public class SalesReporter {
-
+	private double highestSales=0;
+	private double averageSales=0;
+	private SalesAssociate[] team;
+	private int numberOfAssociates;
+	
+	public void getData() {
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
