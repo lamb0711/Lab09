727e943d242f2f3be94a840b02d06e8a53abe1d6
Yang sujin <21500394@handong.edu>
2018-04-10 22:31:29 +0900

    arrange ScalesAssociate class

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -3,19 +3,19 @@ package edu.handong.csee.java.lab09;
 public class SalesAssociate {
 	public String name;
 	public double sales;
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getSales() {
 		return sales;
 	}
-	
+
 	public void setSales(double sales) {
 		this.sales = sales;
 	}
