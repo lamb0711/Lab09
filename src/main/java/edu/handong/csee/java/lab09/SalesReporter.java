6981a2feb0321ed412c6473775712a6c60f2274f
Yang sujin <21500394@handong.edu>
2018-04-12 10:49:19 +0900

    change array to array list

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,5 +1,5 @@
 package edu.handong.csee.java.lab09;//package name
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;//use scanner class
 /**
  * SalesReporter class is save people name and sales to use keyboard. 
@@ -10,7 +10,8 @@ import java.util.Scanner;//use scanner class
 public class SalesReporter {//class name
 	private double highestSales=0;//save highest of sales
 	private double averageSales=0;//save average of sales
-	private HashMap<String,Double> team;//this array save people data. type is salesAssociate 
+	private ArrayList<String> team;//this array save people data. type is salesAssociate 
+	private ArrayList<Double> team2;
 	private int numberOfAssociates;//number of people
 	/**
 	 * getData method is save input data to team array.
@@ -23,16 +24,11 @@ public class SalesReporter {//class name
 		System.out.println("Enter number of sales associates");//print message for input number of people
 		this.numberOfAssociates = Keyboard.nextInt();//save number of people
 
-		team = new HashMap<String,Double>();//allocate team array to use SalesAssociate
+		team = new SalesAssociate[this.numberOfAssociates];//allocate team array to use SalesAssociate
 
-		boolean done = false;
-		int i=0;
-		while(!done) {
-			
-		
-		//for (int i=0; i<this.numberOfAssociates; i++) {//loop for input data
+
+		for (int i=0; i<this.numberOfAssociates; i++) {//loop for input data
 			System.out.println("Enter data for associate number"+(i+1));//it show number of people
-			i++;
 			System.out.print("Enter name of sales associate : ");//print message for input name
 			Keyboard.nextLine();//for eat space
 			name =Keyboard.nextLine();//save input data to String name
@@ -40,14 +36,11 @@ public class SalesReporter {//class name
 			System.out.print("Enter associate's sales: ");//print message
 			sales = Keyboard.nextDouble();//save input data to sales
 
-			//team[i] = new SalesAssociate();//allocate memory to team array
+			team[i] = new SalesAssociate();//allocate memory to team array
 
-			team.put(name, sales);//save input data to team array name //debug
-			//team[i].setSales(sales);//save input data to team array sales
-			System.out.println("Do you want to add one more Salesman?: ");
-			if(Keyboard.nextLine().equalsIgnoreCase("yes"))
-				done = true;
-		//}
+			team[i].setName(name);//save input data to team array name //debug
+			team[i].setSales(sales);//save input data to team array sales
+		}
 
 	}
 	/**
@@ -55,10 +48,10 @@ public class SalesReporter {//class name
 	 */
 	public void computeAverage() {//computeAverage method
 		double sum=0;//declare sum local variable to save sum of sales
-		for(Double sale : team.values()) {//loop for add sales to sum
-			sum = sum + sale;//add sales to sum
+		for(int i=0; i<this.numberOfAssociates; i++) {//loop for add sales to sum
+			sum = sum + team[i].getSales();//add sales to sum
 		}
-		sum = sum/team.size();//sum is divided number of array
+		sum = sum/numberOfAssociates;//sum is divided number of array
 
 		System.out.println("\nAverage Sales per associate is $"+sum);//print Sales average
 		this.averageSales= sum;//save average to averageSales
@@ -67,20 +60,17 @@ public class SalesReporter {//class name
 	 * computehighest method is save highest sales
 	 */
 	public void computehighest() {//computehighest method
-		Double currentHighestValue = 0.0;
-		
-		for(Double currentValue:team.values()) {//loop for save highest sales
-			if(currentHighestValue < currentValue)//if sales is greater than highestSales
-				currentHighestValue = currentValue;//save highest sales to highestSales
+		for(int i=0; i<this.numberOfAssociates; i++) {//loop for save highest sales
+			if(this.highestSales < team[i].getSales())//if sales is greater than highestSales
+				this.highestSales = team[i].getSales();//save highest sales to highestSales
 		}
-		System.out.println("The highest sales figure is $"+currentHighestValue);//print highest sales
+		System.out.println("The highest sales figure is $"+this.highestSales);//print highest sales
 	}
 	/**
 	 * displayResults is print team array
 	 */
 	public void displayResults() {//displayResults method
 		System.out.println("The following had the hightest sales:");//print highest sales people data
-		
 		for(int i=0; i<this.numberOfAssociates; i++) {//add loop for print data
 			if(this.highestSales == team[i].getSales()) {//condition for print highest people
 				System.out.println("Name: "+team[i].getName());//print highest people name
